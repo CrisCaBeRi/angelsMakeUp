f6876fd7ce08b58aa01a5109357b12914b7a19a9
Cristian Camilo Betancourt Rincon <cristian.camilo.betancourt.rin@gmail.com>
2022-12-26 21:54:14 -0500

    Final footer

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,34 +1,41 @@
 import React from "react";
 import "./Footer.css";
+import Carousel from 'react-bootstrap/Carousel';
 
 const Footer = () => {        
     return (
         <>
             <footer>
+                <Carousel className="container_carousel_footer" variant="dark" >      
+                    <Carousel.Item>
+                        <div className="adress">
+                            <h2> Dirección </h2>
+                            <p>Calle 132d # 156 - 14 <br />Bogotá, Suba </p>
+                        </div>
+                    </Carousel.Item>
+                    
+                    <Carousel.Item>
+                    <div className="contact">
+                        <h2>Siguenos en redes</h2>
+                        <div className="social">
+                            <i class="ri-instagram-line"></i>
+                            <i class="ri-facebook-circle-fill"></i>
+                            <i class="ri-whatsapp-line"></i>
+                        </div>                
+                    </div>                      
+                    </Carousel.Item> 
 
-                <div className="adress">
-                    <h2> Dirección </h2>
-                    <p>Calle 132d # 156 - 14 <br />Bogotá, Suba </p>
-                </div>
-
-                <div className="contact">
-                    <h2>Siguenos en redes</h2>
-                    <div className="social">
-                        <i class="ri-instagram-line"></i>
-                        <i class="ri-facebook-circle-fill"></i>
-                        <i class="ri-whatsapp-line"></i>
-                    </div>                
-                </div>
-
-                <div className="about-us">
-                    <h2>Sobre nosotros</h2>
-                    <p>Somos una empresa dedicada al comercio y distribución de artículos de belleza y cuidado personal que opera desde el año 2019.</p>
-                </div>      
+                    <Carousel.Item>
+                        <div className="about-us">
+                            <h2>Sobre nosotros</h2>
+                            <p>Somos una empresa dedicada al comercio y distribución de artículos <br/> de belleza y cuidadopersonal que opera desde el año 2019.</p>
+                        </div> 
+                    </Carousel.Item>
+                </Carousel>                 
                 
-            </footer>           
+            </footer>
         </>     
     )
-
 }
 
 export default Footer; 
